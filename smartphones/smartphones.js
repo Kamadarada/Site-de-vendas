0e2301a0fcb3308acacabf3ecc19904ebd216728
Kamadarada <felipekamada2@gmail.com>
2022-11-26 16:56:31 -0300

    Barra de pesquisa

--- smartphones/smartphones.js
+++ smartphones/smartphones.js
@@ -75,3 +75,21 @@ document.querySelector('#card_cadastro')
         alert('Cadastro concluido');
         window.location = '../index.html';
     }
+
+
+    function buscaproduto() {
+        let input = document.getElementById('searchbar').value
+        input=input.toLowerCase();
+        let x = document.getElementsByClassName('titulo-produto');
+          
+        for (i = 0; i < x.length; i++) { 
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                x[i].style.display="none";
+                
+            }
+            else {
+                x[i].style.display="list-item";    
+                console.log('titulo-produto');             
+            }
+        }
+    }
